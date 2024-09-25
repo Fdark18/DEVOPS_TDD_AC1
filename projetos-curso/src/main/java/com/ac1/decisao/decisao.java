cf761eea60d7d0aa95756b23ac2b8b57b86667fe
Jhonatan Frossard <frossard39@gmail.com>
2024-09-24 23:47:22 -0300

    TDD_AC1

---
+++ projetos-curso/src/main/java/com/ac1/decisao/decisao.java
@@ -0,0 +1,13 @@
+package com.ac1.decisao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class decisao {
+
+	public static void main(String[] args) {
+		SpringApplication.run(decisao.class, args);
+	}
+
+}
---
+++ projetos-curso/src/test/java/com/ac1/decisao/decisaoTests.java
@@ -0,0 +1,13 @@
+package com.ac1.decisao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class decisaoTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
